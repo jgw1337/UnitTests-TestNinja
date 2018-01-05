b11d2c564c049478db21e4b5a890790f645a0443
jgwang-github <jgwang.github@gmail.com>
2018-01-04 22:14:32 -0800

    Leverage SetUp (and optionally TearDown) to streamline coding of Unit Tests

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -6,14 +6,21 @@ namespace TestNinja.UnitTests
     [TestFixture]
     class MathTests
     {
+        private Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
         [Test]
         public void Add_AddTwoNumbers_ReturnsSum()
         {
             // Arrange
-            Math m = new Math();
 
             // Act
-            var result = m.Add(1, 2);
+            var result = _math.Add(1, 2);
 
             // Assert
             Assert.That(result, Is.EqualTo(3));
@@ -22,9 +29,7 @@ namespace TestNinja.UnitTests
         [Test]
         public void Max_1stArgumentIsGreater_Returns1stArgument()
         {
-            Math m = new Math();
-
-            var result = m.Max(2, 1);
+            var result = _math.Max(2, 1);
 
             Assert.That(result, Is.EqualTo(2));
         }
@@ -32,9 +37,7 @@ namespace TestNinja.UnitTests
         [Test]
         public void Max_2ndArgumentIsGreater_Returns2ndArgument()
         {
-            Math m = new Math();
-
-            var result = m.Max(1, 2);
+            var result = _math.Max(1, 2);
 
             Assert.That(result, Is.EqualTo(2));
         }
@@ -42,9 +45,7 @@ namespace TestNinja.UnitTests
         [Test]
         public void Max_BothArgumentsAreEqual_ReturnsSameArgument()
         {
-            Math m = new Math();
-
-            var result = m.Max(1, 1);
+            var result = _math.Max(1, 1);
 
             Assert.That(result, Is.EqualTo(1));
         }
