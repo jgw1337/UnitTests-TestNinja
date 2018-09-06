78598339424b1badbfa7c8f652b45b94c969f96b
jgwang-github <jgwang.github@gmail.com>
2018-09-05 20:44:51 -0700

    Unit Tests for DemeritPointsCalculator

---
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator _calc;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Arrange
+            _calc = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(500)]
+        public void CalculateDemeritPoints_SpeedOutOfBounds_ReturnsError(int value)
+        {
+            // Act and Assert
+            Should.Throw<ArgumentOutOfRangeException>(
+                () => _calc.CalculateDemeritPoints(value)
+            );
+        }
+
+        [Test]
+        [TestCase(10)]
+        [TestCase(65)]
+        public void CalculateDemeritPoints_SpeedLessThanOrEqualToSpeedLimit_ReturnsZero(int value)
+        {
+            // Act
+            var result = _calc.CalculateDemeritPoints(value);
+
+            // Assert
+            result.ShouldBe(0);
+        }
+
+        [Test]
+        [TestCase(70, 1)]
+        public void CalculateDemeritPoints_SpeedExceedsSpeedLimit_ReturnsDemerits(int speed, int demerit)
+        {
+            // Arrange
+
+            // Act
+            var result = _calc.CalculateDemeritPoints(speed);
+
+            // Assert
+            result.ShouldBe(demerit);
+        }
+    }
+}
