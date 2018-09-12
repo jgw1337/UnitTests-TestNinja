360a397d0999a7bdacf9fdf0838b5ec08e6c9453
jgwang-github <jgwang.github@gmail.com>
2018-09-12 15:51:10 -0700

    Initial attempt

---
+++ TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class StackTests
+    {
+        private Stack<string> _fruits;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Arrange
+            _fruits = new Stack<string>();
+            _fruits.Push("apple");
+            _fruits.Push("banana");
+            _fruits.Push("cherry");
+        }
+
+        [Test]
+        public void Count_WhenCalled_ReturnsCountOfList()
+        {
+            // Act
+            var result = _fruits.Count();
+
+            // Assert
+            result.ShouldBe(3);
+        }
+
+        [Test]
+        public void Peek_WhenCalled_ReturnsLastItemOfList()
+        {
+            // Act
+            var result = _fruits.Peek();
+
+            // Assert
+            result.ShouldSatisfyAllConditions(
+                () => _fruits.Count().ShouldBe(3),
+                () => result.ShouldBe("cherry")
+            );
+        }
+
+        [Test]
+        public void Pop_WhenCalled_ReturnsThenRemovesLastItemOfList()
+        {
+            // Act
+            var result = _fruits.Pop();
+
+            // Assert
+            result.ShouldSatisfyAllConditions(
+                () => _fruits.Count().ShouldBe(2),
+                () => result.ShouldBe("cherry")
+            );
+        }
+
+        [Test]
+        public void Push_WithInvalidData_ReturnsException()
+        {
+            // Act and Assert
+            Should.Throw<ArgumentNullException>(
+                () => _fruits.Push(null)
+            );
+        }
+
+        [Test]
+        public void Push_WithValidData_ReturnsVoid()
+        {
+            // Act
+            _fruits.Push("date");
+
+            // Assert
+            _fruits.Count().ShouldBe(4);
+        }
+    }
+}
