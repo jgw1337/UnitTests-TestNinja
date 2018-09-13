5f3c03d892627b970f91c91104fdd4a7b8e61b51
jgwang-github <jgwang.github@gmail.com>
2018-09-12 18:53:20 -0700

    "Interaction Test" Unit Test... which ensures the class we're testing interacts properly with another class

---
+++ TestNinja.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {
+            // Arrange
+            var storage = new Mock<IStorage>();
+            var orderService = new OrderService(storage.Object);
+            var order = new Order();
+
+            // Act
+            orderService.PlaceOrder(order);
+
+            // Assert
+            storage.Verify(x => x.Store(order));
+        }
+    }
+}
